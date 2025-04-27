655830a35b2440f371cc8c9af5ad0a53ae4776a9
RealHonestPete <dillonknuesel@gmail.com>
2025-04-27 11:19:26 -0600

    Add qbitcoin-game/scripts/deploy.js 1745774365210

---
+++ qbitcoin-game/scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const QBitcoin = await ethers.getContractFactory("QBitcoin");
+  const qbitcoin = await QBitcoin.deploy();
+
+  await qbitcoin.deployed();
+
+  console.log("QBitcoin deployed to:", qbitcoin.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
